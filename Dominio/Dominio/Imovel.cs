60aef10db4378bf896c8b9cb1e05cddbdfced260
RodrigoSSoares <srn.rodrigo@gmail.com>
2015-08-19 17:16:15 -0300

    RemoverProduto #GDP-8

---
+++ Dominio/Dominio/Imovel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public enum TipoImovel 
+    {
+        Apartamento, Casa, Loja, Fazenda 
+    }
+    
+    
+    public class Imovel
+    {
+        public int Id { get; set; }
+        public TipoImovel TipoImovel { get; set; }  
+        public string Descricao { get; set; }
+        public string Endereco { get; set; }
+        public byte Quartos { get; set; }
+        public int Valor { get; set; }
+        public string M2Contrucao { get; set; }
+        public string M2Terreno { get; set; }
+    }
+}
---
+++ Dominio/Dominio/Proprietario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Proprietario
+    {
+        public string NomeCompleto { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+    }
+}
